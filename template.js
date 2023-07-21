28081fec50534921952b9a7f92859b37c421216b
Tom Brulin <t.brulin@outlook.com>
2023-07-21 11:43:46 +0200

    initial commit

---
+++ template.js
@@ -0,0 +1,37 @@
+module.exports = `<h1 align="center">Hi 👋, I'm Tom !</h1>
+<h3 align="center">A fullstack developer from France currently focused on the web</h3>
+
+### Contact me
+
+[![Twitter](https://img.shields.io/badge/Twitter-%231DA1F2.svg?logo=Twitter&logoColor=white)](https://twitter.com/Tom_Brulin)
+
+Here is my homemade daytime ☀️ counter before next year ⏱ {day_before_new_years}
+
+#### Languages
+
+![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white) ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white) ![Java](https://img.shields.io/badge/java-f89820.svg?style=for-the-badge) ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) ![PHP](https://img.shields.io/badge/php-777BB4?style=for-the-badge&logo=php&logoColor=fff)
+
+#### Frontend
+
+![Next JS](https://img.shields.io/badge/NextJS-black?style=for-the-badge&logo=next.js&logoColor=white) ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+
+#### Backend
+
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white) ![AdonisJS](https://img.shields.io/badge/AdonisJS-5A45FF?style=for-the-badge&logo=adonisjs&logoColor=white)
+
+#### Others
+
+![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white) ![AWS](https://img.shields.io/badge/aws-232F3E.svg?style=for-the-badge&logo=amazon-aws&logoColor=white)
+
+### My stats 😛
+
+![](https://github-readme-streak-stats.herokuapp.com/?user=tom-brulin&theme=dark&hide_border=true)
+
+### Visitor count
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=tom-brulin&label=Profile%20views&color=0e75b6&style=flat-square" alt="tom-brulin" /> </p>
+
+Last update on {today_date}
+
+🤖 This README.md is updated daily, by Actions ❤️
+`;
